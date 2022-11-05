11b1ca2bc546c96b670ef13748f3c1a65c3a1c3a
Tim Poerschke <post@timkodiert.de>
2022-11-05 09:41:09 +0100

    Erste Logik fürs Hinzufügen von Ausgaben
    (und CSS zur Einstellung der Font entfernt, wegen vorherigen Commit nicht mehr notwendig)

--- src/main/java/timkodiert/budgetBook/view/NewExpenseView.java
+++ src/main/java/timkodiert/budgetBook/view/NewExpenseView.java
@@ -1,5 +1,31 @@
 package timkodiert.budgetBook.view;
 
-public class NewExpenseView {
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class NewExpenseView implements Initializable {
     
+    @FXML
+    private TextField positionTextField, valueTextField; 
+
+    @FXML
+    private TextArea noteTextField;
+
+    @FXML
+    private ChoiceBox<String> typeChoiceBox, month1ChoiceBox, month2ChoiceBox, month2ChoiceBox2, month4ChoiceBox;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+       
+        List<String> typeList = List.of("monatlich", "jährlich", "halbjährlich", "vierteljährlich");
+        typeChoiceBox.getItems().addAll(typeList);
+        // Monatsboxen mit ChangeListener ausblenden
+    }
 }
