23c719275270597e203361b5b7d55085f57b4061
Tim Poerschke <post@timkodiert.de>
2022-11-27 22:07:31 +0100

    EditExpenseView: Zahlungen werden angezeigt

--- src/main/java/timkodiert/budgetBook/view/EditExpenseView.java
+++ src/main/java/timkodiert/budgetBook/view/EditExpenseView.java
@@ -1,6 +1,7 @@
 package timkodiert.budgetBook.view;
 
 import java.net.URL;
+import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.stream.IntStream;
@@ -25,12 +26,14 @@ public class EditExpenseView implements View, Initializable {
     private TextField month1TextField, month2TextField, month3TextField, month4TextField, month5TextField, 
         month6TextField, month7TextField, month8TextField, month9TextField, month10TextField, month11TextField, month12TextField;
 
-    private List<TextField> monthTextFields = List.of(month1TextField, month2TextField, month3TextField, month4TextField, month5TextField, 
-        month6TextField, month7TextField, month8TextField, month9TextField, month10TextField, month11TextField, month12TextField);
+    private List<TextField> monthTextFields;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         positionTextField.setText(expense.getPosition());
+
+        monthTextFields = List.of(month1TextField, month2TextField, month3TextField, month4TextField, month5TextField, 
+            month6TextField, month7TextField, month8TextField, month9TextField, month10TextField, month11TextField, month12TextField);
 
         IntStream.rangeClosed(1, 12).forEach(i -> {
             monthTextFields.get(i-1).setText("" + expense.getValueFor(CURRENT_YEAR, i));
