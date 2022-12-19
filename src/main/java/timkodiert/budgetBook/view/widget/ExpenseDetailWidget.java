d890526309363f88f7f572db451b0bdffb84525f
Tim Poerschke <post@timkodiert.de>
2022-12-19 11:06:42 +0100

    [FEATURE] ExpenseDetailWidget (mit TextFlow)

---
+++ src/main/java/timkodiert/budgetBook/view/widget/ExpenseDetailWidget.java
@@ -0,0 +1,42 @@
+package timkodiert.budgetBook.view.widget;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import timkodiert.budgetBook.domain.model.Expense;
+import timkodiert.budgetBook.domain.model.FixedExpense;
+import timkodiert.budgetBook.view.View;
+
+public class ExpenseDetailWidget implements View, Initializable {
+
+    @FXML
+    private Label positionLabel;
+    @FXML
+    private TextFlow noteTextFlow;
+
+    private Expense expense;
+
+    public ExpenseDetailWidget() {
+        expense = new FixedExpense();
+    }
+
+    public void setExpense(Expense expense) {
+        this.expense = expense;
+        initProperties();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        initProperties();
+    }
+
+    private void initProperties() {
+        positionLabel.textProperty().bind(expense.getAdapter().positionProperty());
+        noteTextFlow.getChildren().setAll(new Text(expense.getNote()));
+    }
+}
