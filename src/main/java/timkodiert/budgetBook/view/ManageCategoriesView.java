84d815d7844b429ec381c8b2a8d53d6c31d34a4b
Tim Poerschke <post@timkodiert.de>
2022-12-25 19:41:13 +0100

    [FEATURE] Kategorien CRUD

--- src/main/java/timkodiert/budgetBook/view/ManageCategoriesView.java
+++ src/main/java/timkodiert/budgetBook/view/ManageCategoriesView.java
@@ -4,11 +4,15 @@ import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
-import javafx.beans.property.ReadOnlyIntegerWrapper;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableColumn;
 import javafx.scene.control.TreeTableView;
@@ -26,14 +30,36 @@ public class ManageCategoriesView implements Initializable {
     @FXML
     private TreeTableColumn<Category, Category> actionColumn;
 
+    @FXML
+    private Node editContainer;
+    @FXML
+    private TextField nameTextField;
+    @FXML
+    private TextArea descriptionTextArea;
+
     private EntityManager em = EntityManager.getInstance();
+
+    private Category selectedCategory;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        categoriesTable.setShowRoot(false);
         nameColumn.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getValue().getName()));
         actionColumn.setCellValueFactory(cellData -> new ReadOnlyObjectWrapper<Category>(cellData.getValue().getValue()));
         actionColumn.setCellFactory(col -> new ButtonTreeTableCell<>("Löschen", this::deleteCategory));
+
+        categoriesTable.getSelectionModel().selectedItemProperty().addListener((ObservableValue<? extends TreeItem<Category>> observable, TreeItem<Category> oldValue, TreeItem<Category> newValue) -> {
+            if(newValue != null) {
+                selectedCategory = newValue.getValue();
+                nameTextField.setText(selectedCategory.getName());
+                descriptionTextArea.setText(selectedCategory.getDescription());
+                editContainer.setDisable(false);
+            }
+            else {
+                editContainer.setDisable(true);
+                nameTextField.setText("");
+                descriptionTextArea.setText("");
+            }
+        });
         
         List<Category> categories = em.findAll(Category.class);
         fillTable(categories);
@@ -58,5 +84,27 @@ public class ManageCategoriesView implements Initializable {
         root.getChildren().addAll(roots);
 
         categoriesTable.setRoot(root);
+    }
+
+    @FXML
+    private void saveCategory(ActionEvent event) {
+        nameTextField.getStyleClass().remove("validation-error");
+
+        if(nameTextField.getText().trim().equals("")) {
+            nameTextField.getStyleClass().add("validation-error");
+        }
+        else {
+            selectedCategory.setName(nameTextField.getText().trim());
+            selectedCategory.setDescription(descriptionTextArea.getText().trim());
+
+            em.persist(selectedCategory);
+            categoriesTable.refresh();
+        }
+    }
+
+    @FXML
+    private void resetCategory(ActionEvent event) {
+        nameTextField.setText(selectedCategory.getName());
+        descriptionTextArea.setText(selectedCategory.getDescription());
     }
 }
