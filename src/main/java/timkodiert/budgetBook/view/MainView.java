84cb9d0fc399115245e76eea9312245d85e2cd35
Tim Poerschke <post@timkodiert.de>
2023-01-03 12:36:39 +0100

    MainView zerlegt, sodass die CompactOverview geladen wird

--- src/main/java/timkodiert/budgetBook/view/MainView.java
+++ src/main/java/timkodiert/budgetBook/view/MainView.java
@@ -9,6 +9,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
@@ -17,6 +18,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import timkodiert.budgetBook.controller.FixedExpenseController;
@@ -26,23 +28,13 @@ import timkodiert.budgetBook.util.EntityManager;
 import timkodiert.budgetBook.util.StageBuilder;
 
 public class MainView implements Initializable {
-    
-    @FXML
-    private TableColumn<ExpenseAdapter, String> monthlyPositionCol, monthlyTypeCol, currentMonthPositionCol, currentMonthTypeCol, nextMonthPositionCol, nextMonthTypeCol;
-    @FXML
-    private TableColumn<ExpenseAdapter, Double> monthlyValueCol, currentMonthValueCol, nextMonthValueCol;
-    @FXML
-    private TableView<ExpenseAdapter> monthlyTable, currentMonthTable, nextMonthTable;
-
-    @FXML 
-    private Label monthlySumLabel, monthlySumLabel1, monthlySumLabel2, currentMonthSumLabel, currentMonthTotalSumLabel, nextMonthSumLabel, nextMonthTotalSumLabel;
 
     private Stage primaryStage;
 
-    private FixedExpenseController fixedExpenseController;
+    @FXML
+    private BorderPane root;
 
     public MainView(Stage primaryStage) throws IOException {
-        this.fixedExpenseController = new FixedExpenseController();
         this.primaryStage = primaryStage;
         this.primaryStage.setOnCloseRequest(windowEvent -> EntityManager.getInstance().closeSession());
 
@@ -56,37 +48,15 @@ public class MainView implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        monthlyPositionCol.setCellValueFactory(new PropertyValueFactory<ExpenseAdapter, String>("position"));
-        monthlyPositionCol.setCellFactory(TextFieldTableCell.forTableColumn());
-        monthlyValueCol.setCellValueFactory(new PropertyValueFactory<ExpenseAdapter, Double>("currentMonthValue"));
-        monthlyValueCol.setCellFactory(col -> new CurrencyTableCell<>());
-        monthlyTypeCol.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().paymentTypeProperty().get().getType()));
-
-        currentMonthPositionCol.setCellValueFactory(new PropertyValueFactory<ExpenseAdapter, String>("position"));
-        currentMonthPositionCol.setCellFactory(TextFieldTableCell.forTableColumn());
-        currentMonthValueCol.setCellValueFactory(new PropertyValueFactory<ExpenseAdapter, Double>("currentMonthValue"));
-        currentMonthValueCol.setCellFactory(col -> new CurrencyTableCell<>());
-        currentMonthTypeCol.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().paymentTypeProperty().get().getType()));
-
-        nextMonthPositionCol.setCellValueFactory(new PropertyValueFactory<ExpenseAdapter, String>("position"));
-        nextMonthPositionCol.setCellFactory(TextFieldTableCell.forTableColumn());
-        nextMonthValueCol.setCellValueFactory(new PropertyValueFactory<ExpenseAdapter, Double>("nextMonthValue"));
-        nextMonthValueCol.setCellFactory(col -> new CurrencyTableCell<>());
-        nextMonthTypeCol.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().paymentTypeProperty().get().getType()));
-
-        // Ausgaben anzeigen
-        monthlyTable.setItems(fixedExpenseController.getMonthlyExpenses());
-        currentMonthTable.setItems(fixedExpenseController.getCurrentMonthExpenses());
-        nextMonthTable.setItems(fixedExpenseController.getNextMonthExpenses());
-
-        // Summen
-        monthlySumLabel.textProperty().bind(fixedExpenseController.monthlyExpensesSumTextProperty());
-        monthlySumLabel1.textProperty().bind(fixedExpenseController.monthlyExpensesSumTextProperty());
-        monthlySumLabel2.textProperty().bind(fixedExpenseController.monthlyExpensesSumTextProperty());
-        currentMonthSumLabel.textProperty().bind(fixedExpenseController.currentMonthExpensesSumTextProperty());
-        currentMonthTotalSumLabel.textProperty().bind(fixedExpenseController.currentMonthExpensesTotalSumTextProperty());
-        nextMonthSumLabel.textProperty().bind(fixedExpenseController.nextMonthExpensesSumTextProperty());
-        nextMonthTotalSumLabel.textProperty().bind(fixedExpenseController.nextMonthExpensesTotalSumTextProperty());
+        // Das Kind laden
+        try {
+            FXMLLoader templateLoader = new FXMLLoader();
+            templateLoader.setLocation(getClass().getResource("/fxml/CompactOverview.fxml"));
+            this.root.setCenter(templateLoader.load());
+        } catch (IOException e) {
+            Alert alert = new Alert(AlertType.ERROR, "Ansicht konnte nicht geöffnet werden!");
+            alert.showAndWait();
+        }
     }
 
     @FXML
@@ -147,7 +117,10 @@ public class MainView implements Initializable {
 
     @FXML
     private void reloadData(ActionEvent event) {
-        fixedExpenseController.loadAll();
-        fixedExpenseController.addNextYearToAllExpenses();
+        // TODO: Über Dependency Injection müsste das doch funktionieren, oder?
+        // Wenn sich alle Views den Controller teilen, könnte man hier aktualisieren
+        // und es wirkt sich auf alle anderen Views aus.
+        //fixedExpenseController.loadAll();
+        //fixedExpenseController.addNextYearToAllExpenses();
     }
 }
