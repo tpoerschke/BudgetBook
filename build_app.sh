d7dd286f7245872653f5c9cce0028ba8da7c1606
Tim Poerschke <post@timkodiert.de>
2023-02-10 21:54:14 +0100

    Shell-Skript baut App

---
+++ build_app.sh
@@ -0,0 +1,19 @@
+mvn clean package || { echo "\n   (╯°□°）╯︵ ┻━┻\n"; exit 1; }
+
+mkdir target/input
+cp target/budget-book-1.0-SNAPSHOT.jar target/input/budget-book-1.0-SNAPSHOT.jar
+
+jpackage --input target/input \
+  --name JPackageDemoApp \
+  --main-jar budget-book-1.0-SNAPSHOT.jar \
+  --type app-image --verbose
+
+jpackage_status=$?
+
+echo "Clean up..."
+rm -rf target/input
+
+if [ $jpackage_status -eq 0 ]; then
+    exit 0
+fi
+exit 1
