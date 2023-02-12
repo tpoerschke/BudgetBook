630293d608d9311f366a1b37d645572393cf3906
Noah Skrzypczyk <49651456+nskrzypczyk@users.noreply.github.com>
2023-02-13 00:16:02 +0100

    feat(project): install.sh + tar.gz für Linux

--- build_app.sh
+++ build_app.sh
@@ -1,20 +1,51 @@
-mvn clean package || { echo "\n   (╯°□°）╯︵ ┻━┻\n"; exit 1; }
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+source .env
+
+rm -rf $APP_NAME
+mvn clean package || {
+  echo "\n   (╯°□°）╯︵ ┻━┻\n"
+  exit 1
+}
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  # Linux
+  icon="images/icon-linux.png"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  # Mac OSX
+  icon="images/mac-icon.icns"
+elif [[ "$OSTYPE" == "msys" ]]; then
+  echo # Windows (MinGW / git bash)
+fi
 
 mkdir target/input
 cp target/budget-book-1.0-SNAPSHOT.jar target/input/budget-book-1.0-SNAPSHOT.jar
 
 jpackage --input target/input \
-  --name JBudgetBook \
-  --icon images/mac-icon.icns \
+  --name $APP_NAME \
+  --icon $icon \
   --main-jar budget-book-1.0-SNAPSHOT.jar \
   --type app-image --verbose
 
 jpackage_status=$?
 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  cp "platform/linux/install.sh" $APP_NAME/
+  cp ".env" $APP_NAME/
+  tar_name="$APP_NAME.tar.gz"
+  rm -rf $tar_name
+  tar -czf $tar_name $APP_NAME
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  echo
+elif [[ "$OSTYPE" == "msys" ]]; then
+  echo
+fi
+
 echo "Clean up..."
 rm -rf target/input
 
 if [ $jpackage_status -eq 0 ]; then
-    exit 0
+  exit 0
 fi
 exit 1
