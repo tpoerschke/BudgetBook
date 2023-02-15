e0284c25df259f5230c62161477b5abaff49c8dd
Tim Poerschke <post@timkodiert.de>
2023-02-15 21:55:49 +0100

    Expenses werden in Tabelle geladen

--- src/main/java/timkodiert/budgetBook/view/ManageExpensesView.java
+++ src/main/java/timkodiert/budgetBook/view/ManageExpensesView.java
@@ -1,17 +1,41 @@
 package timkodiert.budgetBook.view;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javax.inject.Inject;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
+import timkodiert.budgetBook.domain.model.ExpenseAdapter;
+import timkodiert.budgetBook.domain.model.FixedExpense;
+import timkodiert.budgetBook.domain.repository.FixedExpensesRepository;
 
-public class ManageExpensesView implements View {
+public class ManageExpensesView implements View, Initializable {
 
     @FXML
     private Pane detailViewContainer;
 
-    @Inject
-    public ManageExpensesView() {
+    @FXML
+    private TableView<ExpenseAdapter> expensesTable;
 
+    @FXML
+    private TableColumn<ExpenseAdapter, String> positionCol;
+
+    private FixedExpensesRepository fixedExpensesRepository;
+
+    @Inject
+    public ManageExpensesView(FixedExpensesRepository fixedExpensesRepository) {
+        this.fixedExpensesRepository = fixedExpensesRepository;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        positionCol.setCellValueFactory(new PropertyValueFactory<ExpenseAdapter, String>("position"));
+        expensesTable.getItems().addAll(fixedExpensesRepository.findAll().stream().map(FixedExpense::getAdapter).toList());
     }
 }
