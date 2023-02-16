a7c5801cc98c267399ab22c5d23d535282328f45
Noah Skrzypczyk <49651456+nskrzypczyk@users.noreply.github.com>
2023-02-16 23:01:08 +0100

    feat(project): Add uninstall option

--- platform/linux/install.sh
+++ platform/linux/install.sh
@@ -5,23 +5,54 @@ INSTALL_LOCATION="$HOME/$APP_NAME"
 SHORTCUT_PATH="$HOME/.local/share/applications"
 SHORTCUT_LOCATION="$HOME/.local/share/applications/$APP_NAME.desktop"
 ICON_PATH="$INSTALL_LOCATION/lib/$APP_NAME.png"
-rm -rf "$SHORTCUT_LOCATION"
-mkdir -p "$SHORTCUT_PATH"
-mkdir -p $INSTALL_LOCATION
 
-cp -r "bin" "$INSTALL_LOCATION"
-cp -r "lib" "$INSTALL_LOCATION"
+while test $# -gt 0; do
+    case "$1" in
+    -h | --help)
+        echo "Script for installing/removing $APP_NAME"
+        echo " "
+        echo "Usage: ./install.sh [options] [command]"
+        echo " "
+        echo "options:"
+        echo "-h, --help        show this brief help"
+        echo " "
+        echo "commands:"
+        echo "install           install $APP_NAME"
+        echo "remove            remove $APP_NAME from this system"
+        exit 0
+        ;;
+    install)
+        install
+        ;;
+    remove)
+        remove
+        ;;
+    esac
+done
 
-dot_desktop=(
-    "[Desktop Entry]"
-    "Type=Application"
-    "Name=$APP_NAME"
-    "Icon=$ICON_PATH"
-    "Exec=$INSTALL_LOCATION/bin/$APP_NAME"
-    "Categories=Office"
-)
-printf "%s\n" "${dot_desktop[@]}" >"$SHORTCUT_LOCATION"
+install() {
+    rm -rf "$SHORTCUT_LOCATION"
+    mkdir -p "$SHORTCUT_PATH"
+    mkdir -p $INSTALL_LOCATION
 
-chmod +x "$SHORTCUT_LOCATION"
+    cp -r "bin" "$INSTALL_LOCATION"
+    cp -r "lib" "$INSTALL_LOCATION"
 
-xdg-desktop-menu forceupdate
+    dot_desktop=(
+        "[Desktop Entry]"
+        "Type=Application"
+        "Name=$APP_NAME"
+        "Icon=$ICON_PATH"
+        "Exec=$INSTALL_LOCATION/bin/$APP_NAME"
+        "Categories=Office"
+    )
+    printf "%s\n" "${dot_desktop[@]}" >"$SHORTCUT_LOCATION"
+
+    chmod +x "$SHORTCUT_LOCATION"
+
+    xdg-desktop-menu forceupdate
+}
+
+remove() {
+    echo "Will remove $APP_NAME from this system"
+}
