98b22b2a8cb52346d537f33737570fd83a62683f
Tim Poerschke <post@timkodiert.de>
2023-03-30 23:27:44 +0200

    Konzept Summen-/Footertabelle

--- src/main/java/timkodiert/budgetBook/view/MonthlyOverview.java
+++ src/main/java/timkodiert/budgetBook/view/MonthlyOverview.java
@@ -1,29 +1,21 @@
 package timkodiert.budgetBook.view;
 
 import java.net.URL;
-import java.util.List;
 import java.util.ResourceBundle;
 
 import javax.inject.Inject;
 
-import org.kordamp.ikonli.bootstrapicons.BootstrapIcons;
-import org.kordamp.ikonli.javafx.FontIcon;
-
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.ReadOnlyBooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.Pane;
 import timkodiert.budgetBook.domain.model.UniqueExpense;
 import timkodiert.budgetBook.domain.repository.Repository;
 import timkodiert.budgetBook.util.GroupTableCell;
@@ -31,9 +23,9 @@ import timkodiert.budgetBook.util.GroupTableCell;
 public class MonthlyOverview implements Initializable, View {
 
     @FXML
-    private TableView<TableData> dataTable;
+    private TableView<TableData> dataTable, sumTable;
     @FXML
-    private TableColumn<TableData, String> positionCol, valueCol;
+    private TableColumn<TableData, String> positionCol, valueCol, sumTableCol0, sumTableCol1, sumTableCol2;
     @FXML
     private TableColumn<TableData, TableData> buttonCol;
 
@@ -65,43 +57,18 @@ public class MonthlyOverview implements Initializable, View {
             });
         });
 
-        // buttonCol.setCellFactory(col -> {
-        //     Button btn = new Button();
-        //     TableCell<TableData, TableData> cell = new TableCell<>() {
-
-        //         {
-        //             btn.setGraphic(new FontIcon(BootstrapIcons.ARROWS_COLLAPSE));
-        //         }
-
-        //         @Override
-        //         protected void updateItem(TableData item, boolean empty) {
-        //             super.updateItem(item, empty);
-
-        //             if (empty || !item.groupRow()) {
-        //                 this.setGraphic(null);
-        //             } else {
-        //                 //System.out.println(hashCode() + " updateItem isCollapsed " + isCollapsed);
-        //                 this.setGraphic(btn);
-        //             }
-        //         }
-        //     };
-
-        //     btn.setOnAction(event -> {
-        //         if (filteredData.getPredicate() == null) {
-        //             filteredData.setPredicate(d -> {
-        //                 return d.groupRow();
-        //             });
-        //         } else {
-        //             filteredData.setPredicate(null);
-        //         }
-        //     });
-
-        //     return cell;
-        // });
         positionCol.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().position()));
         valueCol.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().value() + "€"));
 
         dataTable.setItems(filteredData);
+
+        sumTableCol0.prefWidthProperty().bind(buttonCol.widthProperty());
+        sumTableCol1.prefWidthProperty().bind(positionCol.widthProperty());
+        sumTableCol2.prefWidthProperty().bind(valueCol.widthProperty());
+
+        sumTable.getItems().add(new TableData("Summe", 99.99, false));
+        sumTableCol1.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().position()));
+        sumTableCol2.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().value() + "€"));
     }
 
     public record TableData(String position, double value, boolean groupRow) {
