ef458e0f23dd44cde3e928dd01bfcd9eb8e7e641
Tim Poerschke <post@timkodiert.de>
2023-04-01 20:35:37 +0200

    WIP Monatsübersicht

--- src/main/java/timkodiert/budgetBook/util/GroupTableCell.java
+++ src/main/java/timkodiert/budgetBook/util/GroupTableCell.java
@@ -9,34 +9,55 @@ import javafx.beans.property.SimpleBooleanProperty;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
+import timkodiert.budgetBook.view.MonthlyOverview.RowType;
 import timkodiert.budgetBook.view.MonthlyOverview.TableData;
 
 public class GroupTableCell extends TableCell<TableData, TableData> {
 
-    private final SimpleBooleanProperty isCollapsedProperty;
+    private final SimpleBooleanProperty isUniqueCollapsedProperty, isFixedCollapsedProperty;
     private final Button btn = new Button();
 
-    public GroupTableCell(SimpleBooleanProperty isCollapsedProperty) {
-        this.isCollapsedProperty = isCollapsedProperty;
-        btn.setGraphic(new FontIcon(BootstrapIcons.ARROWS_COLLAPSE));
+    public GroupTableCell(SimpleBooleanProperty isUniqueCollapsedProperty,
+            SimpleBooleanProperty isFixedCollapsedProperty) {
+        this.isUniqueCollapsedProperty = isUniqueCollapsedProperty;
+        this.isFixedCollapsedProperty = isFixedCollapsedProperty;
         btn.setOnAction(this::handleClick);
     }
 
     private void handleClick(ActionEvent event) {
-        isCollapsedProperty.set(!isCollapsedProperty.get());
-        btn.setGraphic(isCollapsedProperty.get()
-                ? new FontIcon(BootstrapIcons.ARROWS_COLLAPSE)
-                : new FontIcon(BootstrapIcons.ARROWS_EXPAND));
+        TableData item = getItem();
+
+        if (item.type() == RowType.UNIQUE_EXPENSE_GROUP) {
+            isUniqueCollapsedProperty.set(!isUniqueCollapsedProperty.get());
+            btn.setGraphic(isUniqueCollapsedProperty.get()
+                    ? new FontIcon(BootstrapIcons.ARROWS_COLLAPSE)
+                    : new FontIcon(BootstrapIcons.ARROWS_EXPAND));
+        } else if (item.type() == RowType.FIXED_EXPENSE_GROUP) {
+            isFixedCollapsedProperty.set(!isFixedCollapsedProperty.get());
+            btn.setGraphic(isFixedCollapsedProperty.get()
+                    ? new FontIcon(BootstrapIcons.ARROWS_COLLAPSE)
+                    : new FontIcon(BootstrapIcons.ARROWS_EXPAND));
+        }
     }
 
     @Override
     protected void updateItem(TableData item, boolean empty) {
         super.updateItem(item, empty);
 
-        if (empty || !item.groupRow()) {
-            this.setGraphic(null);
-        } else {
+        if (!empty && RowType.getGroupTypes().contains(item.type())) {
             this.setGraphic(btn);
+
+            if (item.type() == RowType.UNIQUE_EXPENSE_GROUP) {
+                btn.setGraphic(isUniqueCollapsedProperty.get()
+                        ? new FontIcon(BootstrapIcons.ARROWS_EXPAND)
+                        : new FontIcon(BootstrapIcons.ARROWS_COLLAPSE));
+            } else if (item.type() == RowType.FIXED_EXPENSE_GROUP) {
+                btn.setGraphic(isFixedCollapsedProperty.get()
+                        ? new FontIcon(BootstrapIcons.ARROWS_EXPAND)
+                        : new FontIcon(BootstrapIcons.ARROWS_COLLAPSE));
+            }
+        } else {
+            this.setGraphic(null);
         }
     }
 }
--- src/main/java/timkodiert/budgetBook/view/MonthlyOverview.java
+++ src/main/java/timkodiert/budgetBook/view/MonthlyOverview.java
@@ -1,11 +1,15 @@
 package timkodiert.budgetBook.view;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javax.inject.Inject;
 
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
@@ -15,9 +19,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.layout.Pane;
+import timkodiert.budgetBook.domain.model.Category;
+import timkodiert.budgetBook.domain.model.FixedExpense;
 import timkodiert.budgetBook.domain.model.UniqueExpense;
 import timkodiert.budgetBook.domain.repository.Repository;
+import timkodiert.budgetBook.util.CurrencyTableCell;
+import timkodiert.budgetBook.util.DateTableCell;
 import timkodiert.budgetBook.util.GroupTableCell;
 
 public class MonthlyOverview implements Initializable, View {
@@ -25,52 +32,106 @@ public class MonthlyOverview implements Initializable, View {
     @FXML
     private TableView<TableData> dataTable, sumTable;
     @FXML
-    private TableColumn<TableData, String> positionCol, valueCol, sumTableCol0, sumTableCol1, sumTableCol2;
+    private TableColumn<TableData, String> positionCol, categoriesCol, sumTableCol0, sumTableCol1;
+    @FXML
+    private TableColumn<TableData, Number> valueCol, sumTableCol2;
+    @FXML
+    private TableColumn<TableData, LocalDate> dateCol;
     @FXML
     private TableColumn<TableData, TableData> buttonCol;
 
-    private Repository<UniqueExpense> uniqueExpenseRepository;
+    private final Repository<UniqueExpense> uniqueExpenseRepository;
+    private final Repository<FixedExpense> fixedExpenseRepository;
 
     @Inject
-    public MonthlyOverview(Repository<UniqueExpense> uniqueExpenseRepository) {
+    public MonthlyOverview(Repository<UniqueExpense> uniqueExpenseRepository,
+            Repository<FixedExpense> fixedExpenseRepository) {
         this.uniqueExpenseRepository = uniqueExpenseRepository;
+        this.fixedExpenseRepository = fixedExpenseRepository;
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         ObservableList<TableData> data = FXCollections.observableArrayList();
-        data.add(new TableData("Einzigartige Ausgaben", 9.99, true));
-        data.addAll(uniqueExpenseRepository.findAll().stream()
-                .map(exp -> new TableData(exp.getBiller(), exp.getTotalValue(), false))
+
+        List<FixedExpense> fixedExpenses = fixedExpenseRepository.findAll();
+        data.add(new TableData("Regelmäßige Ausgaben",
+                fixedExpenses.stream().mapToDouble(FixedExpense::getCurrentMonthValue).sum(),
+                null, null,
+                RowType.FIXED_EXPENSE_GROUP));
+        data.addAll(fixedExpenses.stream()
+                .map(exp -> new TableData(exp.getPosition(), exp.getCurrentMonthValue(), null,
+                        String.join(", ", exp.getCategories().stream().map(Category::getName).toList()),
+                        RowType.FIXED_EXPENSE))
                 .toList());
+
+        List<UniqueExpense> uniqueExpenses = uniqueExpenseRepository.findAll();
+        data.add(new TableData("Einzigartige Ausgaben",
+                uniqueExpenses.stream().mapToDouble(UniqueExpense::getTotalValue).sum(),
+                null, null,
+                RowType.UNIQUE_EXPENSE_GROUP));
+        data.addAll(uniqueExpenses.stream().map(exp -> {
+            String categories = String.join(", ", exp.getPaymentInformations().stream()
+                    .flatMap(info -> info.getCategories().stream()).map(Category::getName).toList());
+            return new TableData(exp.getBiller(), exp.getTotalValue(), exp.getDate(), categories,
+                    RowType.UNIQUE_EXPENSE);
+        }).toList());
+
         FilteredList<TableData> filteredData = new FilteredList<>(data);
 
-        SimpleBooleanProperty isCollapsedProperty = new SimpleBooleanProperty(false);
+        SimpleBooleanProperty isUniqueCollapsedProperty = new SimpleBooleanProperty(false);
+        SimpleBooleanProperty isFixedCollapsedProperty = new SimpleBooleanProperty(true);
+        Runnable predicator = () -> {
+            List<RowType> toShow = new ArrayList<>(RowType.getGroupTypes());
+            if (!isUniqueCollapsedProperty.get()) {
+                toShow.add(RowType.UNIQUE_EXPENSE);
+            }
+            if (!isFixedCollapsedProperty.get()) {
+                toShow.add(RowType.FIXED_EXPENSE);
+            }
+            filteredData.setPredicate(d -> toShow.contains(d.type()));
+        };
+
         buttonCol.setCellValueFactory(cell -> new SimpleObjectProperty<>(cell.getValue()));
-        buttonCol.setCellFactory(col -> new GroupTableCell(isCollapsedProperty));
-        isCollapsedProperty.addListener((observable, oldValue, newValue) -> {
-            filteredData.setPredicate(d -> {
-                if (newValue) {
-                    return d.groupRow();
-                }
-                return true;
-            });
+        buttonCol.setCellFactory(col -> new GroupTableCell(isUniqueCollapsedProperty, isFixedCollapsedProperty));
+        isUniqueCollapsedProperty.addListener((observable, oldValue, newValue) -> {
+            predicator.run();
         });
+        isFixedCollapsedProperty.addListener((observable, oldValue, newValue) -> {
+            predicator.run();
+        });
+        predicator.run();
 
         positionCol.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().position()));
-        valueCol.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().value() + "€"));
+        valueCol.setCellValueFactory(cell -> new SimpleDoubleProperty(cell.getValue().value()));
+        valueCol.setCellFactory(col -> new CurrencyTableCell<>());
+        dateCol.setCellValueFactory(cell -> new SimpleObjectProperty<LocalDate>(cell.getValue().date()));
+        dateCol.setCellFactory(col -> new DateTableCell<>());
+        categoriesCol.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().categories()));
 
+        dataTable.getColumns().forEach(col -> col.setReorderable(false));
         dataTable.setItems(filteredData);
 
         sumTableCol0.prefWidthProperty().bind(buttonCol.widthProperty());
         sumTableCol1.prefWidthProperty().bind(positionCol.widthProperty());
         sumTableCol2.prefWidthProperty().bind(valueCol.widthProperty());
 
-        sumTable.getItems().add(new TableData("Summe", 99.99, false));
+        double totalSum = uniqueExpenses.stream().mapToDouble(UniqueExpense::getTotalValue).sum()
+                + fixedExpenses.stream().mapToDouble(FixedExpense::getCurrentMonthValue).sum();
+        sumTable.getItems().add(new TableData("Summe", totalSum, null, null, RowType.SUM));
         sumTableCol1.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().position()));
-        sumTableCol2.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().value() + "€"));
+        sumTableCol2.setCellValueFactory(cell -> new SimpleDoubleProperty(cell.getValue().value()));
+        sumTableCol2.setCellFactory(col -> new CurrencyTableCell<>());
     }
 
-    public record TableData(String position, double value, boolean groupRow) {
+    public enum RowType {
+        UNIQUE_EXPENSE, FIXED_EXPENSE, UNIQUE_EXPENSE_GROUP, FIXED_EXPENSE_GROUP, SUM;
+
+        public static List<RowType> getGroupTypes() {
+            return List.of(UNIQUE_EXPENSE_GROUP, FIXED_EXPENSE_GROUP);
+        }
+    }
+
+    public record TableData(String position, double value, LocalDate date, String categories, RowType type) {
     }
 }
