f48ad84af93d91587ad8483b84c07c51cfbc178b
Noah Skrzypczyk <49651456+nskrzypczyk@users.noreply.github.com>
2023-11-20 22:42:07 +0100

    #22: Introduce fallback language (English)

--- src/main/java/timkodiert/budgetBook/i18n/LanguageManager.java
+++ src/main/java/timkodiert/budgetBook/i18n/LanguageManager.java
@@ -4,22 +4,36 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 public class LanguageManager {
+
+    private static final String I_18_N_PACKAGE = "i18n.messages";
+
+    private final ResourceBundle fallbackRB;
 
     private ResourceBundle resourceBundle;
 
     private Locale locale;
 
-    public synchronized void setLocale(Locale locale){
-        this.locale = locale;
-        this.resourceBundle = ResourceBundle.getBundle("i18n.messages", this.locale);
+    public LanguageManager(Locale locale) {
+        this.resourceBundle = ResourceBundle.getBundle(I_18_N_PACKAGE, locale);
+        this.fallbackRB = ResourceBundle.getBundle(I_18_N_PACKAGE, Locale.ENGLISH);
     }
 
-    public String getLocString(String key){
-        return this.resourceBundle.getString(key);
+    public synchronized void setLocale(Locale locale) {
+        this.locale = locale;
+        this.resourceBundle = ResourceBundle.getBundle(I_18_N_PACKAGE, this.locale);
+    }
+
+    public String getLocString(String key) {
+        if (resourceBundleContainsKey(key)) {
+            return this.getResourceBundle().getString(key);
+        }
+        return this.getFallbackRB().getString(key);
+    }
+
+    private boolean resourceBundleContainsKey(String key) {
+        return this.resourceBundle.containsKey(key);
     }
 }
--- src/test/java/timkodiert/budgetBook/i18n/LanguageManagerTest.java
+++ src/test/java/timkodiert/budgetBook/i18n/LanguageManagerTest.java
@@ -3,13 +3,20 @@ package timkodiert.budgetBook.i18n;
 import java.util.Locale;
 import java.util.stream.Stream;
 
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class LanguageManagerTest {
+
     static Stream<Arguments> testArgs(){
         return Stream.of(
                 Arguments.of(Locale.GERMAN, "Willkommen"),
@@ -21,8 +28,14 @@ class LanguageManagerTest {
     @MethodSource("testArgs")
     void should_return_the_string_for_specified_language_and_key(Locale locale, String expectedString){
         String greetingKey = "main.greeting";
-        LanguageManager languageManager = new LanguageManager();
+        LanguageManager languageManager = new LanguageManager(locale);
         languageManager.setLocale(locale);
         assertEquals(expectedString, languageManager.getLocString(greetingKey));
+    }
+
+    @Test
+    void should_use_the_fallback_ResourceBundle_if_current_one_does_not_contain_key(){
+        LanguageManager languageManager = new LanguageManager(Locale.GERMAN);
+        assertEquals("Fallback", languageManager.getLocString("testing.fallback"));
     }
 }
