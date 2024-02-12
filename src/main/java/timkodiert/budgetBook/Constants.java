7229d37cc6125ce932fd9273b50efc35cf02b545
Tim Poerschke <post@timkodiert.de>
2024-02-12 10:27:24 +0100

    Verschiedene OperationalMode mit eigenen Properties-Dateien implementiert

--- src/main/java/timkodiert/budgetBook/Constants.java
+++ src/main/java/timkodiert/budgetBook/Constants.java
@@ -1,12 +1,13 @@
 package timkodiert.budgetBook;
 
 import java.nio.file.Path;
-import java.util.List;
 
 public class Constants {
         
         public static final String INITIAL_AMOUNT_STRING = "0,00 €";
 
+        public static final String OPERATION_MODE_ARGUMENT_NAME = "mode";
+
         public static final String DATA_DIR = ".budgetbook";
-        public static final String PROPERTIES_PATH = Path.of(System.getProperty("user.home"), DATA_DIR, "application.properties").toString();
+        public static final String PROPERTIES_PATH_TEMPLATE = Path.of(System.getProperty("user.home"), DATA_DIR, "application%s.properties").toString();
 }
--- src/main/java/timkodiert/budgetBook/Main.java
+++ src/main/java/timkodiert/budgetBook/Main.java
@@ -4,9 +4,12 @@ import atlantafx.base.theme.Theme;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import timkodiert.budgetBook.util.OperationMode;
 import timkodiert.budgetBook.util.PropertiesService;
 import timkodiert.budgetBook.view.DaggerViewComponent;
 import timkodiert.budgetBook.view.ViewComponent;
+
+import static timkodiert.budgetBook.Constants.OPERATION_MODE_ARGUMENT_NAME;
 
 /**
  * Hello world!
@@ -16,8 +19,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        PropertiesService.getInstance().load();
-        Class<? extends Theme> theme = PropertiesService.getInstance().getTheme();
+        PropertiesService propsService = PropertiesService.getInstance();
+        Parameters params = getParameters();
+        if (params.getNamed().containsKey(OPERATION_MODE_ARGUMENT_NAME)) {
+            OperationMode operationMode = OperationMode.valueOf(params.getNamed().get(OPERATION_MODE_ARGUMENT_NAME));
+            propsService.setOperationMode(operationMode);
+        }
+        propsService.load();
+        Class<? extends Theme> theme = propsService.getTheme();
         Application.setUserAgentStylesheet(theme.getConstructor().newInstance().getUserAgentStylesheet());
         ViewComponent viewComponent = DaggerViewComponent.create();
         viewComponent.getMainView().setAndShowPrimaryStage(primaryStage);
