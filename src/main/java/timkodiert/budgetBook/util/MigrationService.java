e864141bafd1724470c0dad5806036950ecb79d9
Tim Poerschke <post@timkodiert.de>
2024-03-03 20:49:01 +0100

    MigrationService implementiert

--- src/main/java/timkodiert/budgetBook/util/MigrationService.java
+++ src/main/java/timkodiert/budgetBook/util/MigrationService.java
@@ -1,27 +1,59 @@
 package timkodiert.budgetBook.util;
 
+import java.util.Set;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import javafx.application.Platform;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import lombok.Getter;
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.callback.Callback;
+import org.flywaydb.core.api.callback.Context;
+import org.flywaydb.core.api.callback.Event;
 
 import timkodiert.budgetBook.i18n.LanguageManager;
 import timkodiert.budgetBook.util.dialog.StackTraceAlert;
 import timkodiert.budgetBook.view.ControllerFactory;
-import timkodiert.budgetBook.view.ViewComponent;
 
 @Singleton
 public class MigrationService {
 
-    private final ViewComponent viewComponent;
     private final ControllerFactory controllerFactory;
 
+    private final Flyway flyway;
+
+    @Getter
+    private int pendingCount = 0;
+    private final StringProperty currentScript = new SimpleStringProperty();
+    private final IntegerProperty numMigrated = new SimpleIntegerProperty();
+    private final BooleanProperty migrationFinished = new SimpleBooleanProperty();
+    private final BooleanProperty migrationError = new SimpleBooleanProperty();
+
     @Inject
-    public MigrationService(ViewComponent viewComponent, ControllerFactory controllerFactory) {
-        this.viewComponent = viewComponent;
+    public MigrationService(ControllerFactory controllerFactory) {
         this.controllerFactory = controllerFactory;
+        flyway = Flyway.configure()
+                       .dataSource(PropertiesService.getInstance().getDbPath(), "sa", "")
+                       .callbacks(new NotifierCallback())
+                       .load();
+    }
+
+    public boolean hasPendingMigrations() {
+        return flyway.info().pending().length > 0;
+    }
+
+    public void migrate() {
+        pendingCount = flyway.info().pending().length;
+        flyway.migrate();
     }
 
     public void show() {
@@ -35,10 +67,57 @@ public class MigrationService {
             stage.setTitle("Migration – JBudgetBook");
             stage.setScene(new Scene(viewLoader.load()));
             stage.setWidth(600);
+            stage.setOnHidden(event -> {
+                Platform.exit();
+                System.exit(0);
+            });
             stage.showAndWait();
         } catch (Exception e) {
             StackTraceAlert.of(LanguageManager.get("alert.viewCouldNotBeOpened"), e).showAndWait();
         }
     }
 
+    public StringProperty currentScriptProperty() {
+        return currentScript;
+    }
+
+    public IntegerProperty numMigratedProperty() {
+        return numMigrated;
+    }
+
+    public BooleanProperty migrationFinishedProperty() {
+        return migrationFinished;
+    }
+
+    public BooleanProperty migrationErrorProperty() {
+        return migrationError;
+    }
+
+    private class NotifierCallback implements Callback {
+
+        @Override
+        public boolean supports(Event event, Context context) {
+            return Set.of(Event.BEFORE_EACH_MIGRATE, Event.AFTER_EACH_MIGRATE, Event.AFTER_MIGRATE_ERROR, Event.AFTER_MIGRATE_OPERATION_FINISH).contains(event);
+        }
+
+        @Override
+        public boolean canHandleInTransaction(Event event, Context context) {
+            return true; // Irrelevant, da nicht mit der Database interagiert wird
+        }
+
+        @Override
+        public void handle(Event event, Context context) {
+            switch (event) {
+                case BEFORE_EACH_MIGRATE -> currentScript.set(context.getMigrationInfo().getScript());
+                case AFTER_EACH_MIGRATE -> numMigrated.set(numMigrated.get() + 1);
+                case AFTER_MIGRATE_ERROR -> migrationError.set(true);
+                case AFTER_MIGRATE_OPERATION_FINISH -> migrationFinished.set(true);
+            }
+        }
+
+        @Override
+        public String getCallbackName() {
+            return NotifierCallback.class.getSimpleName();
+        }
+    }
 }
--- src/main/java/timkodiert/budgetBook/view/MigrationView.java
+++ src/main/java/timkodiert/budgetBook/view/MigrationView.java
@@ -1,9 +1,57 @@
 package timkodiert.budgetBook.view;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javax.inject.Inject;
 
-public class MigrationView implements View {
+import javafx.beans.binding.Bindings;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import timkodiert.budgetBook.util.MigrationService;
+import timkodiert.budgetBook.util.dialog.DialogFactory;
+
+public class MigrationView implements View, Initializable {
+
+    @FXML
+    private Label progressIndicatorText;
+    @FXML
+    private Label progressLabel;
+    @FXML
+    private Button startButton;
+
+    private final MigrationService service;
+    private final DialogFactory dialogFactory;
 
     @Inject
-    public MigrationView() {}
+    public MigrationView(MigrationService service, DialogFactory dialogFactory) {
+        this.service = service;
+        this.dialogFactory = dialogFactory;
+    }
+
+    @FXML
+    private void startMigration(ActionEvent event) {
+        startButton.setDisable(true);
+        service.migrate();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        progressLabel.textProperty().bind(service.currentScriptProperty());
+        progressIndicatorText.textProperty()
+                             .bind(Bindings.createStringBinding(() -> String.format("%d / %d", service.numMigratedProperty().get(), service.getPendingCount()),
+                                                                service.numMigratedProperty()));
+
+        service.migrationFinishedProperty().addListener((observableValue, oldVal, newVal) -> {
+            ((Stage) startButton.getScene().getWindow()).close();
+        });
+
+        service.migrationErrorProperty().addListener((observableValue, oldVal, newVal) -> {
+            dialogFactory.buildErrorDialog("Es ist ein Migrationsfehler aufgetreten.").showAndWait();
+        });
+    }
 }
