7b0a7957064bac01561f3ab786e2f99f3f76e34b
Tim Poerschke <post@timkodiert.de>
2024-03-30 13:41:55 +0100

    Datum, wenn wiederkehrende Ausgabe importiert wurde + Icon (für alle importierten Ausgaben)

--- src/main/java/timkodiert/budgetBook/table/monthlyOverview/TableData.java
+++ src/main/java/timkodiert/budgetBook/table/monthlyOverview/TableData.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 import timkodiert.budgetBook.util.HasType;
 import timkodiert.budgetBook.view.MonthlyOverview.RowType;
 
-public record TableData(String position, double value, LocalDate date, String categories, RowType type) implements HasType<RowType> {
+public record TableData(String position, double value, LocalDate date, String categories, boolean hasImport, RowType type) implements HasType<RowType> {
 
     @Override
     public RowType getType() {
