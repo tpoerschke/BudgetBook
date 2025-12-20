d615ee87446ad0bb45a625d1f50a6073d9b8cf2c
Tim Poerschke <post@timkodiert.de>
2025-12-20 17:26:42 +0100

    Rundungsfehler beim Import behoben

--- bb.contract/src/main/java/timkodiert/budgetbook/importer/CsvAmountToIntegerConverter.java
+++ bb.contract/src/main/java/timkodiert/budgetbook/importer/CsvAmountToIntegerConverter.java
@@ -1,15 +1,12 @@
 package timkodiert.budgetbook.importer;
 
-import java.text.NumberFormat;
 import java.text.ParseException;
-import java.util.Locale;
 
 import com.opencsv.bean.AbstractBeanField;
 import com.opencsv.exceptions.CsvDataTypeMismatchException;
 
-// Eigentlich müsste man die Annotationen aus der AccountTurnover-Entity lösen
-// und eine Bean mit den Annotationen im BL-Modul anlegen, dann kann auch dieser
-// Converter hier verschoben werden
+import timkodiert.budgetbook.util.MoneyEssentials;
+
 public class CsvAmountToIntegerConverter extends AbstractBeanField<Integer, String> {
 
     @Override
@@ -20,8 +17,7 @@ public class CsvAmountToIntegerConverter extends AbstractBeanField<Integer, Stri
 
         try {
             // NumberFormat nicht als Konstante, da Probleme mit Multi-Threading
-            double number = NumberFormat.getInstance(Locale.GERMANY).parse(value.trim()).doubleValue();
-            return (int)(number * 100);
+            return MoneyEssentials.asBigDecimalFromGermanStr(value.trim()).multiply(MoneyEssentials.FACTOR_100).intValueExact();
         } catch (ParseException | NumberFormatException e) {
             throw new CsvDataTypeMismatchException(e.getMessage());
         }
