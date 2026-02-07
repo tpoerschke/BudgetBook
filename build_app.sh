f6cce98282065da260ea712142537cf6a7fb6c43
NOAH <skrzypczyk.noah@gmail.com>
2026-02-07 19:51:05 +0100

    #121: Restrukturiere build_app.sh

--- build_app.sh
+++ build_app.sh
@@ -2,52 +2,96 @@
 # -*- coding: utf-8 -*-
 
 source .env
+APP_VERSION="${APP_VERSION//BETA-/0.}"
+echo ${APP_VERSION}
 
-rm -rf $APP_NAME
-mvn clean package || {
-  echo "\n   (╯°□°）╯︵ ┻━┻\n"
-  exit 1
+BUILD_DIR="./BUILD"
+
+
+# -------- BASE METHODS --------
+
+jpackage_win() {
+  echo "starting JPackage..."
+  jpackage --input target/input \
+    --name "${APP_NAME}" \
+    --icon "${icon}" \
+    --main-jar "${MAIN_JAR}" \
+    --app-version "${APP_VERSION}" \
+    --dest "${BUILD_DIR}" \
+    --type msi \
+    --win-per-user-install
+    --verbose
 }
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  # Linux
-  icon="images/icon-linux.png"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  # Mac OSX
-  icon="images/mac-icon.icns"
-elif [[ "$OSTYPE" == "msys" ]]; then
-  echo # Windows (MinGW / git bash)
-fi
+jpackage_linux_rpm() {
+  echo "starting JPackage..."
+  jpackage --input target/input \
+    --name "${APP_NAME}" \
+    --icon "${icon}" \
+    --main-jar "${MAIN_JAR}" \
+    --app-version "${APP_VERSION}" \
+    --type msi \
+    --verbose
+}
 
-mkdir target/input
-cp bb.application/target/$MAIN_JAR target/input/$MAIN_JAR
+clean_output_dir() {
+  rm -rf $BUILD_DIR
+}
 
-echo "starting JPackage..."
-jpackage --input target/input \
-  --name "${APP_NAME}" \
-  --icon "${icon}" \
-  --main-jar "${MAIN_JAR}" \
-  --type msi \
-  --verbose
+run_maven_build() {
+  mvn clean package || {
+    echo "\n   (╯°□°）╯︵ ┻━┻\n"
+    exit 1
+  }
+}
 
-jpackage_status=$?
+make_build_dir_and_copy_target() {
+  mkdir target/input
+  cp bb.application/target/$MAIN_JAR target/input/$MAIN_JAR
+}
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  cp "platform/linux/install.sh" $APP_NAME/
-  cp ".env" $APP_NAME/
-  tar_name="$APP_NAME.tar.gz"
-  rm -rf $tar_name
-  tar -czf $tar_name $APP_NAME
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  echo
-elif [[ "$OSTYPE" == "msys" ]]; then
-  echo
-fi
+# -------- MAIN --------
 
-echo "Clean up..."
-rm -rf target/input
+main() {
+  clean_output_dir
+  # run_maven_build
+  make_build_dir_and_copy_target
 
-if [ $jpackage_status -eq 0 ]; then
-  exit 0
-fi
-exit 1
+
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Linux
+    icon="images/icon-linux.png"
+    jpackage_linux_rpm
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # Mac OSX
+    icon="images/mac-icon.icns"
+  elif [[ "$OSTYPE" == "msys" ]]; then
+    # Windows (MinGW / git bash)
+    jpackage_win
+  fi
+
+  jpackage_status=$?
+  if [ $jpackage_status -eq 0 ]; then
+    exit 0
+  fi
+
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    cp "platform/linux/install.sh" $APP_NAME/
+    cp ".env" $APP_NAME/
+    tar_name="$APP_NAME.tar.gz"
+    rm -rf $tar_name
+    tar -czf $tar_name $APP_NAME
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo
+  elif [[ "$OSTYPE" == "msys" ]]; then
+    echo
+  fi
+
+  echo "Clean up..."
+  rm -rf target/input
+
+  exit 1
+
+}
+
+main
