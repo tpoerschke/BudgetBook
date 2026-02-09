1b61164a7bc13392384f71831c0ff72fe30fbdd6
Tim Poerschke <post@timkodiert.de>
2026-02-09 21:17:02 +0100

    Kostenlosen Runner gewählt + Ergänzung in prepare_release.sh

--- prepare_release.sh
+++ prepare_release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-read -r -p "Wurde die Release-Version bereits in der .env aktualisiert und commitet? (y/n) " answer
+read -r -p "Wurde die Release-Version bereits in der .env-Datei aktualisiert und commitet? (y/n) " answer
 if [[ "$answer" != "y" ]];
   then echo "Release-Vorbereitung abgebrochen"
   exit 1
@@ -13,3 +13,4 @@ mvn release:prepare || {
 }
 
 echo "Release-Vorbereitung abgeschlossen. Beim nächsten Push wird eine Github-Action das Release abwickeln."
+echo "Die Datei release.properties kann gelöscht werden, sofern die Vorbereitungen erfolgreich waren."
