4d44b0637ffc309e07d9c44a9e1560d34527047e
Tim Poerschke <post@timkodiert.de>
2026-02-09 18:15:54 +0100

    FIX prepare_release.sh

--- prepare_release.sh
+++ prepare_release.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-# Hinweis und Abfrage, ob zuerst die Versionen in der .env aktualisiert und commitet wurden
-# mvn release:prepare ausführen
-
 read -r -p "Wurde die Release-Version bereits in der .env aktualisiert und commitet? (y/n) " answer
 if [[ "$answer" != "y" ]];
   then echo "Release-Vorbereitung abgebrochen"
   exit 1
 fi
 
-mvn clean package || {
+mvn release:prepare || {
   echo "Release-Vorbereitung abgebrochen"
   exit 1
 }
 
-echo "Release-Vorbereitung abgeschlossen. Beim nächsten Push wird eine Github-Action das Release abwickeln"
+echo "Release-Vorbereitung abgeschlossen. Beim nächsten Push wird eine Github-Action das Release abwickeln."
